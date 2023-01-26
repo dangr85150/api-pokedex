340ef547517e55c1eb1a29b7cc7e7201598cdd59
dangr <dang85150@gmail.com>
2023-01-26 18:06:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const Sequelize = require('./src/db/sequelize')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app
+.use(favicon(__dirname + '/favicon.ico'))
+.use(bodyParser.json())
+
+Sequelize.initDb()
+
+app.get('/', (req, res) => {
+  res.json('Hello Firebase !')
+})
+
+// ici nous placerons nos futurs points de terminaison.
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
+require('./src/routes/login')(app)
+
+// on ajoute la gestion des erreurs 404
+app.use(({res}) => {
+  const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
+  res.status(404).json({message})
+})
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://${port}`))
